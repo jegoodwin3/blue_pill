4596c20b2b06a3315854f2fa8a3d6814327c7759
John Goodwin <jegoodwin3@gmail.com>
2016-03-29 10:57:05 -0700

    add samples

---
+++ dotfiles/fetch_samples.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+GIT_HOME=$(echo "/c/Users/jgoodwin3")
+cd $GIT_HOME/blue_pill
+if [ ! -n "$GIT_HOME/blue_pill/samples" ]; then
+  mkdir $GIT_HOME/blue_pill/samples
+fi
+cd ./samples
+curl -O http://www.quickstepapps.com/wp-content/uploads/2014/10/awsscripted.zip
+curl -O http://www.quickstepapps.com/wp-content/uploads/2015/06/awsscripted2.zip
+# TDD samples
